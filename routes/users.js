ad8df7c8f231805ac2c461ce17ba7cd33906de85
Nguyen Van Duy <duynv@evolableasia.vn>
2018-11-30 15:15:56 +0700

    first change

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource abc');
+  res.send('respond with a resource i cant do anything');
 });
 
 module.exports = router;
